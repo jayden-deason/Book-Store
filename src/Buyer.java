057468353c632b9618af592c005ebc632672c023
jayden-deason <ironfist4657@gmail.com>
2022-11-10 20:29:14 -0500

    added implementation for addProductToCart and getShoppingCart added index field and getter

--- src/Buyer.java
+++ src/Buyer.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Buyer
@@ -10,6 +11,7 @@ import java.util.ArrayList;
  * @version
  */
 public class Buyer extends User {
+    int index;
     File customers = new File("customers.csv");
     /**
      * Creates a new src.Buyer with a given username & password, and an empty shopping cart
@@ -23,9 +25,10 @@ public class Buyer extends User {
             try (BufferedReader bfr = new BufferedReader(new FileReader("customers.csv"))) {
                 String line = bfr.readLine();
                 while (line != null) {
-                    index++;
+                    index = Integer.parseInt(line.split(",")[0]) + 1;
                     line = bfr.readLine();
                 }
+                this.index = index;
                 FileOutputStream fos = new FileOutputStream("customers.csv", true);
                 PrintWriter pw = new PrintWriter(fos);
                 pw.printf("%d,%s,%s,<>\n", index, this.getUsername(), this.getPassword());
@@ -49,19 +52,66 @@ public class Buyer extends User {
 
     /**
      * Get the buyer's shopping cart contents
-     * @return the list of products in the buyer's shopping cart
+     * @return the list of products in the buyer's shopping cart in the form index:quantity
      */
-    public ArrayList<Product> getShoppingCart() {
-       // TODO: implement with file i/o
-        return new ArrayList<Product>();
+    public ArrayList<String> getShoppingCart() {
+        ArrayList<String> cartItems = new ArrayList<String>();
+        String cart = "";
+        try (BufferedReader bfr = new BufferedReader(new FileReader("customers.csv"))) {
+            String line = bfr.readLine();
+            while (line != null) {
+                if (Integer.parseInt(line.split(",")[0]) == this.getIndex()) {
+                    cart = line.split(",")[3].substring(1, line.split(",")[3].length() - 1);
+                }
+                line = bfr.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] cartArray = cart.split("/");
+        Collections.addAll(cartItems, cartArray);
+        return cartItems;
     }
 
     /**
      * Add a product to the shopping cart
      * @param product the product to be added
+     * @param quantity the number of products to be added
      */
-    public void addProductToCart(Product product) {
-        // TODO: implement with file i/o
+    public void addProductToCart(Product product, int quantity) {
+        ArrayList<String> customerInfo = new ArrayList<String>();
+        try (BufferedReader bfr = new BufferedReader(new FileReader("customers.csv"))) {
+            String line = bfr.readLine();
+            while (line != null) {
+                customerInfo.add(line);
+                line = bfr.readLine();
+            }
+            FileOutputStream fos = new FileOutputStream("customers.csv", false);
+            PrintWriter pw = new PrintWriter(fos);
+            for (int i = 0; i < customerInfo.size(); i++) {
+                String[] lineArray = customerInfo.get(i).split(",");
+                if (Integer.parseInt(lineArray[0]) == this.getIndex()) {
+                    if (lineArray[3].length() > 2) {
+                        pw.printf("%s,%s,%s,%s\n", lineArray[0], lineArray[1], lineArray[2],
+                                String.format("%s/%d:%d>", lineArray[3].substring(0, lineArray[3].length() - 1),
+                                        product.getIndex(), quantity));
+                    } else {
+                        pw.printf("%s,%s,%s,%s\n", lineArray[0], lineArray[1], lineArray[2],
+                                String.format("%s%d:%d>", lineArray[3].substring(0, lineArray[3].length() - 1),
+                                        product.getIndex(), quantity));
+                    }
+                } else {
+                    pw.println(customerInfo.get(i));
+                }
+            }
+            pw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getIndex() {
+        return index;
     }
 
     /**
