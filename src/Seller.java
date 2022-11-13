751b1fed59d0a65712643ef047bcc87a77c6f6c1
VisvShah <visvshah1@gmail.com>
2022-11-13 01:55:54 -0500

    Now updating Seller.csv when adding a store

--- src/Seller.java
+++ src/Seller.java
@@ -113,6 +113,11 @@ public class Seller extends User {
             }
             int lastIndex = Integer.parseInt(lines.get(lines.size() - 1).split(",")[0]);
             s.setIndex(lastIndex);
+            int updateSellerFile = updateSellerFile(lastIndex);
+            if(updateSellerFile == -1) {
+                System.out.println("Something went wrong with updating your profile!");
+                return -1;
+            }
             String line = lastIndex + "," + s.getName() + "," + super.getUsername() + ",<";
             for (Product product : s.getProducts()) {
                 line += product.getIndex() + ":" + product.getQuantity() + "/";
@@ -120,6 +125,69 @@ public class Seller extends User {
             line = line.substring(0, line.length() - 1) + ">";
             fw.write(line + "\n");
             return lastIndex;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+        finally {
+            if(fw != null) {
+                try {
+                    fw.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                    return -1;
+                }
+            }
+        }
+    }
+    /**
+     * Updates Seller.csv when a new store is added, by storing a reference to that store in the seller's row.
+     * @params indexToAdd : states the index of the new store in the Store.csv file
+     */
+    public int updateSellerFile(int indexToAdd) {
+        ArrayList<String> lines = new ArrayList<String>();
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader("Seller.csv"));
+            String line = br.readLine();
+            while(line != null) {
+                if(line.split(",")[1].equals(this.getUsername())) {
+                    String[] parts = line.split(",");
+                    String storesStringArray = parts[3].substring(1, parts[3].length() - 1) + "/" + indexToAdd;
+                    String newLine = String.format("%s,%s,%s,%s", parts[0], parts[1], parts[2], storesStringArray);
+                    lines.add(newLine);
+                }
+                else {
+                    lines.add(line);
+                }
+
+                line = br.readLine();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+        finally {
+            if(br != null) {
+                try {
+                    br.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                    return -1;
+                }
+            }
+        }
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter("Seller.csv");
+            for(String str : lines) {
+                fw.write(str + "\n");
+            }
+            return 0;
         }
         catch (Exception e) {
             e.printStackTrace();
