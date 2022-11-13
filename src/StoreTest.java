c013a2e08fe3602bf922ca6e5af30c2e77661bf1
VisvShah <visvshah1@gmail.com>
2022-11-13 17:41:08 -0500

    Added function

--- src/StoreTest.java
+++ src/StoreTest.java
@@ -13,8 +13,8 @@ public class StoreTest {
             BufferedReader bfr = new BufferedReader(new FileReader(file));
             for (String line = bfr.readLine(); line != null; line = bfr.readLine()) {
                 String[] splitLine = line.split(",");
-                stores.add(new Store(Integer.parseInt(splitLine[0]), splitLine[1], splitLine[2], "Products.csv",
-                        Integer.parseInt(splitLine[3]), Integer.parseInt(splitLine[4])));
+                //stores.add(new Store(Integer.parseInt(splitLine[0]), splitLine[1], splitLine[2], "Products.csv",
+                        //Integer.parseInt(splitLine[3]), Integer.parseInt(splitLine[4])));
             }
         } catch (IOException e) {
             e.printStackTrace();
