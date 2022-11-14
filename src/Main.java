d5d8d034aacfd189146188522aac66928a7095c9
kteodorovich <katyateodorovich@gmail.com>
2022-11-14 12:56:42 -0500

    tiny change to make commit work

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         Market market = Market.getInstance();
 
 //        while (true) {
