59f854bfedbdfd0c46d15afff7a7cd2681f308a3
VisvShah <visvshah1@gmail.com>
2022-11-13 20:04:09 -0500

    Formatted Seller.java

--- src/Seller.java
+++ src/Seller.java
@@ -15,20 +15,21 @@ import java.io.FileWriter;
  */
 public class Seller extends User {
     private ArrayList<Store> stores; // the seller's products
-    private int index;
+    private int index; // the index of the Seller in the Seller.csv file
 
     /**
      * Create  a new seller with a given username, password, and list of products
      * When a seller gets created using this constructor, it gets added to the Sellers.csv file
      * @param username the seller's username
      * @param password the seller's password
+     * 
      */
-    public Seller(String username, String password) throws badNamingException{
+    public Seller(String username, String password) throws badNamingException {
         super(username, password);
-        if(username.contains(",")) {
+        if (username.contains(",")) {
             throw new badNamingException("Please do not have a comma in your username!");
         }
-        if(password.contains(",")) {
+        if (password.contains(",")) {
             throw new badNamingException("Please do not have a comma in your password!");
         }
         this.stores = new ArrayList<Store>();
@@ -49,9 +50,9 @@ public class Seller extends User {
         try {
             br = new BufferedReader(new FileReader("Stores.csv"));
             String l = br.readLine();
-            while(l != null) {
-                for(String n : storesIndex) {
-                    if(Integer.parseInt(n) == i) {
+            while (l != null) {
+                for (String n: storesIndex) {
+                    if (Integer.parseInt(n) == i) {
                         Store s = new Store(l);
                         this.stores.add(s);
                         break;
@@ -60,16 +61,13 @@ public class Seller extends User {
                 l = br.readLine();
                 i++;
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally {
-            if(br != null) {
+        } finally {
+            if (br != null) {
                 try {
                     br.close();
-                }
-                catch (IOException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
@@ -81,29 +79,26 @@ public class Seller extends User {
      */
     public int writeStoresFile(String fileName, Store s) {
 
-        ArrayList<String> lines = new ArrayList<String>();
+        ArrayList < String > lines = new ArrayList<String>();
 
         File f = new File(fileName);
-        if(f.exists()) {
+        if (f.exists()) {
             BufferedReader br = null;
             try {
                 br = new BufferedReader(new FileReader(fileName));
                 String line = br.readLine();
-                while(line != null) {
+                while (line != null) {
                     lines.add(line);
                     line = br.readLine();
                 }
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
                 return -1;
-            }
-            finally {
-                if(br != null) {
+            } finally {
+                if (br != null) {
                     try {
                         br.close();
-                    }
-                    catch (IOException e) {
+                    } catch (IOException e) {
                         e.printStackTrace();
                         return -1;
                     }
@@ -114,37 +109,34 @@ public class Seller extends User {
         FileWriter fw = null;
         try {
             fw = new FileWriter(fileName);
-            for(String str : lines) {
+            for (String str: lines) {
                 fw.write(str + "\n");
             }
             int lastIndex = 0;
-            if(lines.size() != 1) {
+            if (lines.size() != 1) {
                 Integer.parseInt(lines.get(lines.size() - 1).split(",")[0]);
             }
             s.setIndex(lastIndex);
             int updateSellerFile = updateSellerFile(lastIndex);
-            if(updateSellerFile == -1) {
+            if (updateSellerFile == -1) {
                 System.out.println("Something went wrong with updating your profile!");
                 return -1;
             }
             String line = lastIndex + "," + s.getName() + "," + super.getUsername() + ",<";
-            for (Product product : s.getProducts()) {
+            for (Product product: s.getProducts()) {
                 line += product.getIndex() + ":" + product.getQuantity() + "/";
             }
             line = line.substring(0, line.length() - 1) + ">";
             fw.write(line + "\n");
             return lastIndex;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return -1;
-        }
-        finally {
-            if(fw != null) {
+        } finally {
+            if (fw != null) {
                 try {
                     fw.close();
-                }
-                catch (IOException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                     return -1;
                 }
@@ -161,30 +153,26 @@ public class Seller extends User {
         try {
             br = new BufferedReader(new FileReader("Seller.csv"));
             String line = br.readLine();
-            while(line != null) {
-                if(line.split(",")[1].equals(this.getUsername())) {
+            while (line != null) {
+                if (line.split(",")[1].equals(this.getUsername())) {
                     String[] parts = line.split(",");
                     String storesStringArray = parts[3].substring(1, parts[3].length() - 1) + "/" + indexToAdd;
                     String newLine = String.format("%s,%s,%s,%s", parts[0], parts[1], parts[2], storesStringArray);
                     lines.add(newLine);
-                }
-                else {
+                } else {
                     lines.add(line);
                 }
 
                 line = br.readLine();
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return -1;
-        }
-        finally {
-            if(br != null) {
+        } finally {
+            if (br != null) {
                 try {
                     br.close();
-                }
-                catch (IOException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                     return -1;
                 }
@@ -193,21 +181,18 @@ public class Seller extends User {
         FileWriter fw = null;
         try {
             fw = new FileWriter("Seller.csv");
-            for(String str : lines) {
+            for (String str: lines) {
                 fw.write(str + "\n");
             }
             return 0;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return -1;
-        }
-        finally {
-            if(fw != null) {
+        } finally {
+            if (fw != null) {
                 try {
                     fw.close();
-                }
-                catch (IOException e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                     return -1;
                 }
@@ -218,27 +203,24 @@ public class Seller extends User {
      * Exports a csv of all the products in a store that a seller has.
      * @return the index of the store
      */
-    public void exportProducts(String fileName, String storeName){
-        for(Store s: stores) {
-            if(s.getName().equals(storeName)) {
+    public void exportProducts(String fileName, String storeName) {
+        for (Store s: stores) {
+            if (s.getName().equals(storeName)) {
                 FileWriter fw = null;
                 try {
                     fw = new FileWriter(fileName);
                     int index = 0;
-                    for(Product product : s.getProducts()) {
+                    for (Product product: s.getProducts()) {
                         fw.write(index + "," + product.toString() + "\n");
                         index++;
                     }
-                }
-                catch (Exception e) {
+                } catch (Exception e) {
                     e.printStackTrace();
-                }
-                finally {
-                    if(fw != null) {
+                } finally {
+                    if (fw != null) {
                         try {
                             fw.close();
-                        }
-                        catch (IOException e) {
+                        } catch (IOException e) {
                             e.printStackTrace();
                         }
                     }
@@ -259,9 +241,9 @@ public class Seller extends User {
      * seller. Then, It will call the functions to save that store in the stores.csv and update the seller.csv row
      * with a reference to the new store.
      */
-    public void addStore(String storeName){
-        for(Store store : stores) {
-            if(store.getName().equals(storeName)) {
+    public void addStore(String storeName) {
+        for (Store store: stores) {
+            if (store.getName().equals(storeName)) {
                 System.out.println("Error: You already have a store with the same name!");
                 return;
             }
@@ -271,10 +253,9 @@ public class Seller extends User {
         //                 String productSales
         stores.add(s);
         int index = writeStoresFile("Stores.csv", s);
-        if(index == -1) {
+        if (index == -1) {
             System.out.println("Something went wrong with adding your store!");
-        }
-        else {
+        } else {
             s.setIndex(index);
         }
     }
@@ -287,7 +268,7 @@ public class Seller extends User {
      *                 wit
      */
     public void printDashboard(int sortType) {
-        for(Store s : stores) {
+        for (Store s: stores) {
             System.out.println("Store: " + s.getName());
             s.statisticsForSeller(sortType);
         }
@@ -297,23 +278,22 @@ public class Seller extends User {
      */
     public String toString() {
         String storesStr = "<";
-        for(Store s: this.stores) {
+        for (Store s: this.stores) {
             storesStr += s.getIndex() + "/";
         }
         storesStr = storesStr.substring(storesStr.length() - 1) + ">";
         return String.format("%d,%s,%s,%s", this.getIndex(), this.getUsername(), this.getPassword(), storesStr);
 
     }
-    public static void main(String[] args) throws badNamingException{
+    public static void main(String[] args) throws badNamingException {
         //Testing the creation of a seller
         Seller s1 = new Seller("0,bob@gmail.com,bob123,<1/2>");
         System.out.println(s1 + "==" + "0,bob@gmail.com,bob123,<1/2> : " + s1.toString().equals("0,bob@gmail.com," +
                 "bob123,<1/2>"));
         //Testing the creation of a seller and an edge case that comes when creating a Seller with a "," in the
         try {
-            Seller s2 = new Seller("bob@gmail.com" , "bob,123");
-        }
-        catch (badNamingException e) {
+            Seller s2 = new Seller("bob@gmail.com", "bob,123");
+        } catch (badNamingException e) {
             System.out.println("badNamingException thrown for password: bob,123");
         }
         //Testing the printDashBoard without a sort
@@ -327,15 +307,15 @@ public class Seller extends User {
     public int getIndex() {
         return index;
     }
-    public void viewProductsInCart(ArrayList<Buyer> buyers, ArrayList<Product> products) {
+    public void viewProductsInCart(ArrayList < Buyer > buyers, ArrayList < Product > products) {
         System.out.println("In Customer Carts: ");
-        for(Buyer b : buyers) {
+        for (Buyer b: buyers) {
             ArrayList<String> items = b.getShoppingCart();
-            for(int i = 0; i < items.size(); i++) {
+            for (int i = 0; i < items.size(); i++) {
                 int number = 1;
-                for(Store s : stores) {
+                for (Store s: stores) {
                     Product p = products.get(Integer.parseInt(items.get(i).split(":")[0]));
-                    if(p.getStoreName().equals(s.getName())) {
+                    if (p.getStoreName().equals(s.getName())) {
                         System.out.println(number + ".)");
                         number++;
                         System.out.println("Product: " + p.getName() + "| Quantity: " + items.get(i).split(":")[1]);
