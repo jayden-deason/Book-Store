921754c3a57f23c1e6a7915d1f27bfb64aeb68da
VisvShah <visvshah1@gmail.com>
2022-12-02 19:33:12 -0500

    Created Login Logic
    -For both buyers and sellers in server

--- src/Server.java
+++ src/Server.java
@@ -3,9 +3,52 @@ import java.net.*;
 import java.util.ArrayList;
 public class Server extends Thread{
     private final Socket socket;
-    public void run(Socket s) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter writer = new PrintWriter(socket.getOutputStream());
+    public static Market market;
+    public void run() {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            PrintWriter writer = new PrintWriter(this.socket.getOutputStream());
+            //loginString format: (0 for seller or 1 for buyer),(0 for sign in or 1 for sign up),email,password
+            //Example: "0,1,test123@t.com,123" should be sign up for seller with email:test123@t.com and password 123
+            String loginString = reader.readLine();
+            String[] userDetails = loginString.split(",");
+            //Seller
+            if(userDetails[0].equals("0")) {
+                if(userDetails[1].equals("0")) {
+                    Buyer b = market.getBuyerByEmail(userDetails[2]);
+                    if(b.getPassword().equals(userDetails[3])) {
+                        //Sends Client "Y" to signify logged in correctly
+                        writer.println("Y");
+                    }
+                    else{
+                        //Sends Client "N" to signify an error
+                        writer.println("N");
+                    }
+                }
+                if(userDetails[1].equals("1")) {
+
+                }
+            }
+            //Buyer
+            else if(userDetails[0].equals("1")) {
+                if(userDetails[1].equals("0")) {
+                    Seller s = market.getSellerByEmail(userDetails[2]);
+                    if(s.getPassword().equals(userDetails[3])) {
+                        //Sends Client "Y" to signify logged in correctly
+                        writer.println("Y");
+                    }
+                    else{
+                        //Sends Client "N" to signify an error
+                        writer.println("N");
+                    }
+                }
+                if(userDetails[1].equals("1")) {
+
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         //log in or sign up
         while(true){
             //infinitely waits for user to make requests to access data and sends data back to user
@@ -26,4 +69,5 @@ public class Server extends Thread{
     public Server(Socket socket) {
         this.socket = socket;
     }
+    
 }
