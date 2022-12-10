51d0db3955128a96be12952661610bde42da920b
kteodorovich <katyateodorovich@gmail.com>
2022-12-10 17:40:45 -0500

    removed old dependencies

--- src/ClientTest.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class ClientTest {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Invalid Username or Password." +
-                "\nEnter correct info or create an account.", "Error", JOptionPane.ERROR_MESSAGE);
-    }
-}
